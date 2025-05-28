9921bcaec9110af9396245370b4329574befc980
unknown <23114290016@ACADEMICO.IESB.BR>
2025-05-27 21:29:14 -0300

    revisar 13-crud

---
+++ 13-crud/src/alunos/AlunosLista.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AlunosLista() {
+  return (
+    <View>
+      <Text>AlunosLista</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
